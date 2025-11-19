f3d4cda3a2ddb9d8414da9ef0e530502a7dcc68c
dncdnl <danicadaniela.cesicar@gmail.com>
2025-11-19 19:48:00 +0800

    Added SQL Server connection objects in the ClubRegistrationQuery

--- SQLselect-Insert-and-Update-Lab-Exer-1/ClubRegistrationQuery.cs
+++ SQLselect-Insert-and-Update-Lab-Exer-1/ClubRegistrationQuery.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace SQLselect_Insert_and_Update_Lab_Exer_1
 {
     internal class ClubRegistrationQuery
     {
+        // SQL Server connection objects
+        private SqlConnection sqlConnect;
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlAdapter;
+        private SqlDataReader sqlReader;
+
     }
 }
