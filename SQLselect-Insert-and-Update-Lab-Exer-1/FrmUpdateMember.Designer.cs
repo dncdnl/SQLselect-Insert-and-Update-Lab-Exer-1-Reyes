8dda37b0bdba6fe5b2c74d78f7a91553f2cf78d4
dncdnl <danicadaniela.cesicar@gmail.com>
2025-11-19 19:26:17 +0800

    UI Created for FrmUpdateMember

---
+++ SQLselect-Insert-and-Update-Lab-Exer-1/FrmUpdateMember.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SQLselect_Insert_and_Update_Lab_Exer_1
+{
+    partial class FrmUpdateMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStuID2 = new System.Windows.Forms.Label();
+            this.cbStuNO = new System.Windows.Forms.ComboBox();
+            this.lblLastN2 = new System.Windows.Forms.Label();
+            this.tbLN2 = new System.Windows.Forms.TextBox();
+            this.lblFN2 = new System.Windows.Forms.Label();
+            this.tbFN2 = new System.Windows.Forms.TextBox();
+            this.lblMN2 = new System.Windows.Forms.Label();
+            this.tbMN2 = new System.Windows.Forms.TextBox();
+            this.lblAge2 = new System.Windows.Forms.Label();
+            this.tbAge2 = new System.Windows.Forms.TextBox();
+            this.lblGender2 = new System.Windows.Forms.Label();
+            this.cbGender2 = new System.Windows.Forms.ComboBox();
+            this.lblProgram = new System.Windows.Forms.Label();
+            this.cbProgram2 = new System.Windows.Forms.ComboBox();
+            this.butConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStuID2
+            // 
+            this.lblStuID2.AutoSize = true;
+            this.lblStuID2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStuID2.Location = new System.Drawing.Point(12, 9);
+            this.lblStuID2.Name = "lblStuID2";
+            this.lblStuID2.Size = new System.Drawing.Size(103, 15);
+            this.lblStuID2.TabIndex = 1;
+            this.lblStuID2.Text = "Student ID: ";
+            // 
+            // cbStuNO
+            // 
+            this.cbStuNO.FormattingEnabled = true;
+            this.cbStuNO.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.cbStuNO.Location = new System.Drawing.Point(15, 27);
+            this.cbStuNO.Name = "cbStuNO";
+            this.cbStuNO.Size = new System.Drawing.Size(185, 21);
+            this.cbStuNO.TabIndex = 10;
+            // 
+            // lblLastN2
+            // 
+            this.lblLastN2.AutoSize = true;
+            this.lblLastN2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastN2.Location = new System.Drawing.Point(12, 61);
+            this.lblLastN2.Name = "lblLastN2";
+            this.lblLastN2.Size = new System.Drawing.Size(95, 15);
+            this.lblLastN2.TabIndex = 11;
+            this.lblLastN2.Text = "Last Name: ";
+            // 
+            // tbLN2
+            // 
+            this.tbLN2.Location = new System.Drawing.Point(15, 79);
+            this.tbLN2.Name = "tbLN2";
+            this.tbLN2.Size = new System.Drawing.Size(154, 20);
+            this.tbLN2.TabIndex = 12;
+            // 
+            // lblFN2
+            // 
+            this.lblFN2.AutoSize = true;
+            this.lblFN2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFN2.Location = new System.Drawing.Point(12, 112);
+            this.lblFN2.Name = "lblFN2";
+            this.lblFN2.Size = new System.Drawing.Size(103, 15);
+            this.lblFN2.TabIndex = 13;
+            this.lblFN2.Text = "First Name: ";
+            // 
+            // tbFN2
+            // 
+            this.tbFN2.Location = new System.Drawing.Point(15, 130);
+            this.tbFN2.Name = "tbFN2";
+            this.tbFN2.Size = new System.Drawing.Size(154, 20);
+            this.tbFN2.TabIndex = 14;
+            // 
+            // lblMN2
+            // 
+            this.lblMN2.AutoSize = true;
+            this.lblMN2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMN2.Location = new System.Drawing.Point(12, 162);
+            this.lblMN2.Name = "lblMN2";
+            this.lblMN2.Size = new System.Drawing.Size(111, 15);
+            this.lblMN2.TabIndex = 15;
+            this.lblMN2.Text = "Middle Name: ";
+            // 
+            // tbMN2
+            // 
+            this.tbMN2.Location = new System.Drawing.Point(15, 180);
+            this.tbMN2.Name = "tbMN2";
+            this.tbMN2.Size = new System.Drawing.Size(154, 20);
+            this.tbMN2.TabIndex = 16;
+            // 
+            // lblAge2
+            // 
+            this.lblAge2.AutoSize = true;
+            this.lblAge2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge2.Location = new System.Drawing.Point(12, 214);
+            this.lblAge2.Name = "lblAge2";
+            this.lblAge2.Size = new System.Drawing.Size(47, 15);
+            this.lblAge2.TabIndex = 17;
+            this.lblAge2.Text = "Age: ";
+            // 
+            // tbAge2
+            // 
+            this.tbAge2.Location = new System.Drawing.Point(15, 232);
+            this.tbAge2.Name = "tbAge2";
+            this.tbAge2.Size = new System.Drawing.Size(92, 20);
+            this.tbAge2.TabIndex = 18;
+            // 
+            // lblGender2
+            // 
+            this.lblGender2.AutoSize = true;
+            this.lblGender2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender2.Location = new System.Drawing.Point(12, 266);
+            this.lblGender2.Name = "lblGender2";
+            this.lblGender2.Size = new System.Drawing.Size(71, 15);
+            this.lblGender2.TabIndex = 19;
+            this.lblGender2.Text = "Gender: ";
+            // 
+            // cbGender2
+            // 
+            this.cbGender2.FormattingEnabled = true;
+            this.cbGender2.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.cbGender2.Location = new System.Drawing.Point(15, 284);
+            this.cbGender2.Name = "cbGender2";
+            this.cbGender2.Size = new System.Drawing.Size(121, 21);
+            this.cbGender2.TabIndex = 20;
+            // 
+            // lblProgram
+            // 
+            this.lblProgram.AutoSize = true;
+            this.lblProgram.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgram.Location = new System.Drawing.Point(12, 318);
+            this.lblProgram.Name = "lblProgram";
+            this.lblProgram.Size = new System.Drawing.Size(79, 15);
+            this.lblProgram.TabIndex = 21;
+            this.lblProgram.Text = "Program: ";
+            // 
+            // cbProgram2
+            // 
+            this.cbProgram2.FormattingEnabled = true;
+            this.cbProgram2.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.cbProgram2.Location = new System.Drawing.Point(15, 336);
+            this.cbProgram2.Name = "cbProgram2";
+            this.cbProgram2.Size = new System.Drawing.Size(185, 21);
+            this.cbProgram2.TabIndex = 22;
+            // 
+            // butConfirm
+            // 
+            this.butConfirm.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butConfirm.Location = new System.Drawing.Point(58, 383);
+            this.butConfirm.Name = "butConfirm";
+            this.butConfirm.Size = new System.Drawing.Size(99, 37);
+            this.butConfirm.TabIndex = 23;
+            this.butConfirm.Text = "Confirm";
+            this.butConfirm.UseVisualStyleBackColor = true;
+            // 
+            // FrmUpdateMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(223, 450);
+            this.Controls.Add(this.butConfirm);
+            this.Controls.Add(this.cbProgram2);
+            this.Controls.Add(this.lblProgram);
+            this.Controls.Add(this.cbGender2);
+            this.Controls.Add(this.lblGender2);
+            this.Controls.Add(this.tbAge2);
+            this.Controls.Add(this.lblAge2);
+            this.Controls.Add(this.tbMN2);
+            this.Controls.Add(this.lblMN2);
+            this.Controls.Add(this.tbFN2);
+            this.Controls.Add(this.lblFN2);
+            this.Controls.Add(this.tbLN2);
+            this.Controls.Add(this.lblLastN2);
+            this.Controls.Add(this.cbStuNO);
+            this.Controls.Add(this.lblStuID2);
+            this.Name = "FrmUpdateMember";
+            this.Text = "FrmUpdateMember";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStuID2;
+        private System.Windows.Forms.ComboBox cbStuNO;
+        private System.Windows.Forms.Label lblLastN2;
+        private System.Windows.Forms.TextBox tbLN2;
+        private System.Windows.Forms.Label lblFN2;
+        private System.Windows.Forms.TextBox tbFN2;
+        private System.Windows.Forms.Label lblMN2;
+        private System.Windows.Forms.TextBox tbMN2;
+        private System.Windows.Forms.Label lblAge2;
+        private System.Windows.Forms.TextBox tbAge2;
+        private System.Windows.Forms.Label lblGender2;
+        private System.Windows.Forms.ComboBox cbGender2;
+        private System.Windows.Forms.Label lblProgram;
+        private System.Windows.Forms.ComboBox cbProgram2;
+        private System.Windows.Forms.Button butConfirm;
+    }
+}
---
+++ SQLselect-Insert-and-Update-Lab-Exer-1/FrmUpdateMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLselect_Insert_and_Update_Lab_Exer_1
+{
+    public partial class FrmUpdateMember : Form
+    {
+        public FrmUpdateMember()
+        {
+            InitializeComponent();
+        }
+    }
+}
