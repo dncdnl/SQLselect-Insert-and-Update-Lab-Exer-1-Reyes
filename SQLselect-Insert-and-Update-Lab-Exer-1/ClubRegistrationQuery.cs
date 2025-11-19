88370a5ad67145473eeb2367c485c8eb18559b0e
dncdnl <danicadaniela.cesicar@gmail.com>
2025-11-19 19:43:46 +0800

    Made a Club Registration Query class

---
+++ SQLselect-Insert-and-Update-Lab-Exer-1/ClubRegistrationQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLselect_Insert_and_Update_Lab_Exer_1
+{
+    internal class ClubRegistrationQuery
+    {
+    }
+}
